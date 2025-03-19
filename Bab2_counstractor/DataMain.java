3b40813dd1e58476dceefe30db6bfaf61470e474
Muhammad-Farid-Fadillah <arfanhera@gmail.com>
2025-03-20 00:40:18 +0700

    Add files via upload

---
+++ Bab2_counstractor/DataMain.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectmotor.Bab2;
+
+/**
+ *
+ * @author dedys
+ */
+public class DataMain {
+    public static void main(String[] args){
+        // Membuat objek Motor
+        Dealermotor motor1 = new Dealermotor("Honda CBR 2013", "Biru", "2025", 35000000, 10);
+        Dealermotor motor2 = new Dealermotor("Yamaha RX King", "Hitam", "2024", 35000000, 10);
+        
+        // Menampilkan informasi motor
+        motor1.tampildata1();
+        motor2.tampildata1();
+        
+        // Mengganti data motor
+        motor2.warna = ("Kuning");
+        
+        // Menampilkan informasi motor
+        System.out.println("Data berhasil Di ubah");
+        motor1.tambahStok(10);
+        motor1.tampildata1();
+        motor2.kurangiStok(5);
+        motor2.tampildata1();
+    }
+}
---
+++ Bab2_counstractor/Dealermotor.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectmotor.Bab2;
+
+/**
+ *
+ * @author dedys
+ */
+public class Dealermotor {
+    String merk;
+    String warna;
+    String tahunmasuk;
+    int harga;
+    int jumlah;
+    
+//    untuk menginisialisasikan object motor
+    public Dealermotor(String merk,String warna,String tahunmasuk,int harga,int jumlah){
+        this.merk=merk;
+        this.warna=warna;
+        this.jumlah=jumlah;
+        this.tahunmasuk=tahunmasuk;
+        this.harga=harga;
+    }
+    
+//    method atau fungsi untul menampilkan data awal motor
+    public void tampildata1(){
+        System.out.println("Data Motor Dalam Dealer Kami");
+        System.out.println("----------------------------");
+        System.out.println("Merk : " + merk);
+        System.out.println("Warna : " + warna);
+        System.out.println("Jumlah : " + jumlah);
+        System.out.println("Tahun Masuk : " + tahunmasuk);
+        System.out.println("Harga : " + harga);
+        System.out.println(" ");
+}
+    public void gantiWarna(String warnaBaru){
+        System.out.println(" Warna baru untuk " + merk + " adalah " + warna);
+        warna = warnaBaru;
+        System.out.println(" Warna baru untuk " + merk + " adalah " + warna);
+    }
+    // Method untuk menambah stok motor
+    public void tambahStok(int jumlahBaru) {
+        jumlah += jumlahBaru;
+        System.out.println(jumlahBaru + " unit motor " + merk + " telah ditambahkan. Total stok: " + jumlah);
+    }
+
+    // Method untuk mengurangi stok motor saat penjualan
+    public void kurangiStok(int jumlahTerjual) {
+        if (jumlahTerjual > jumlah) {
+            System.out.println("Stok tidak mencukupi! Tersedia hanya " + jumlah + " unit.");
+        } else {
+            jumlah -= jumlahTerjual;
+            System.out.println(jumlahTerjual + " unit motor " + merk + " telah terjual. Sisa stok: " + jumlah);
+        }
+    }
+
+    // Method untuk menghitung total harga dari semua unit motor yang tersedia
+    public int hitungTotalHarga() {
+        return jumlah * harga;
+    }
+}
